2999af2f67e0b2849c1e3e3b85c369bc4760ac20
Snape <serg.serg2006@mail.ru>
2025-07-07 22:18:01 +0300

    Write a wrapper that invokes commands

---
+++ src/handler.py
@@ -0,0 +1,65 @@
+import csv
+import pathlib
+from operator import attrgetter
+from typing import Sequence
+
+from tabulate import tabulate
+
+from commands.base import BaseCommand
+from common_types import Data
+from exceptions import IncorrectDataException
+
+
+class FileHandler:
+
+    @staticmethod
+    def output_data(
+        data: Data,
+        headers: Sequence[str],
+    ) -> None:
+        print(
+            tabulate(
+                [row.values() for row in data],
+                headers=headers,
+                tablefmt="psql",
+            )
+        )
+
+    def __init__(
+        self,
+        file_path: str,
+        values: dict[str, str],
+    ) -> None:
+        self.path = pathlib.Path(file_path).resolve()
+        if not self.path.exists():
+            raise IncorrectDataException(
+                message="Файл не найден. "
+                "Пожалуйста, передайте полный путь."
+            )
+        self.values = values
+        self.handlers = list[BaseCommand]()
+
+    def handle(self):
+        data = Data()
+        fieldnames = tuple[str]()
+        with self.path.open() as file:
+            reader = csv.DictReader(file)
+            fieldnames = reader.fieldnames
+            data = list(reader)
+        self.handlers.sort(key=attrgetter("number_in_queue"))
+        for handler in self.handlers:
+            value = self.values.get(handler.command, None)
+            if value is not None:
+                data, fieldnames = handler.handle_data(
+                    current_data=data,
+                    fieldnames=fieldnames,
+                    value=value,
+                )
+        self.output_data(data, fieldnames)
+
+    def register_handler(self, command: BaseCommand) -> None:
+        if not isinstance(command, BaseCommand):
+            raise TypeError(
+                "Команда должна быть наследником BaseCommand"
+            )
+        self.handlers.append(command)
